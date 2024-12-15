5f687140dfbd887d0e87d479c347333b8bb913a7
Jacek Korzemski <wrp40.000@wp.pl>
2024-12-15 16:51:42 +0100

    fixed roads in dungeons

--- src/c/RandomDungeonEncounterGenerator.tsx
+++ src/c/RandomDungeonEncounterGenerator.tsx
@@ -1,30 +1,39 @@
 import { useState } from "react";
-import { rre } from "../assets/data/randomRoadEncounters";
+import { rde } from "../assets/data/randomDungeonEncounteres";
 
 const RandomDungeonEncounterGenerator = () => {
   const [number, setNumber] = useState<number>(5);
-  const [show, setShow] = useState<boolean>(false);
+  const [events, setEvents] = useState<string[]>([]);
 
-  const randomEncounter = () => {
-    const randomIndex = Math.floor(Math.random() * rre.length);
-    return rre[randomIndex];
-  }
+  const randomEncounters = () => {
+    return Array.from({ length: number }).map(() => {
+      const randomIndex = Math.floor(Math.random() * rde.length);
+      return rde[randomIndex];
+  })}
 
-  return <>
-  <h1>Randon Dungeon Events</h1>
-  <hr/>
-    <button onClick={() => {setNumber(number + 1); setShow(false)}}>+</button>
-    <button onClick={() => setShow(true)}>Generate {number}</button>
-    <button onClick={() => {setNumber(number - 1); setShow(false)}}>-</button>
-    <hr/>
-    {show && <>
-      {Array.from({ length: number }).map((_, index) => (
-        <div key={index}>
-          {index + 1} : {randomEncounter()}
-        </div>
-      ))}
-    </>}
-  </>
-}
+  const handleGenerate = () => {
+    setEvents(randomEncounters());
+  };
+
+  return (
+    <>
+      <h1>Random Dungeon Events</h1>
+      <hr />
+      <button onClick={() => { setNumber(number + 1); }}>+</button>
+      <button onClick={handleGenerate}>Generate {number}</button>
+      <button onClick={() => { setNumber(number - 1); }}>-</button>
+      <hr />
+      {events.length > 0 && (
+        <>
+          {events.map((event, index) => (
+            <div key={index}>
+              {index + 1} : {event}
+            </div>
+          ))}
+        </>
+      )}
+    </>
+  );
+};
 
 export default RandomDungeonEncounterGenerator;
--- src/c/RandomRoadEncounterGenerator.tsx
+++ src/c/RandomRoadEncounterGenerator.tsx
@@ -3,27 +3,37 @@ import { rre } from "../assets/data/randomRoadEncounters";
 
 const RandomRoadEncounterGenerator = () => {
   const [number, setNumber] = useState<number>(5);
-  const [show, setShow] = useState<boolean>(false);
+  const [events, setEvents] = useState<string[]>([]);
 
-  const randomEncounter = () => {
-    const randomIndex = Math.floor(Math.random() * rre.length);
-    return rre[randomIndex];
-  }
+  const randomEncounters = () => {
+    return Array.from({ length: number }).map(() => {
+      const randomIndex = Math.floor(Math.random() * rre.length);
+      return rre[randomIndex];
+  })}
 
-  return <>
-  <h1>Randon Road Events</h1>
-    <button onClick={() => {setNumber(number + 1); setShow(false)}}>+</button>
-    <button onClick={() => setShow(true)}>Generate {number}</button>
-    <button onClick={() => {setNumber(number - 1); setShow(false)}}>-</button>
-    <hr/>
-    {show && <>
-      {Array.from({ length: number }).map((_, index) => (
-        <div key={index}>
-          {index + 1} : {randomEncounter()}
-        </div>
-      ))}
-    </>}
-  </>
+  const handleGenerate = () => {
+    setEvents(randomEncounters());
+  };
+
+  return (
+    <>
+      <h1>Random Dungeon Events</h1>
+      <hr />
+      <button onClick={() => { setNumber(number + 1); }}>+</button>
+      <button onClick={handleGenerate}>Generate {number}</button>
+      <button onClick={() => { setNumber(number - 1); }}>-</button>
+      <hr />
+      {events.length > 0 && (
+        <>
+          {events.map((event, index) => (
+            <div key={index}>
+              {index + 1} : {event}
+            </div>
+          ))}
+        </>
+      )}
+    </>
+  );
 }
 
 export default RandomRoadEncounterGenerator;
