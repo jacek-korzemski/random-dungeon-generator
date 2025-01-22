82a0fffb3407c57b84639bc7d3cd9a2d6909955e
Jacek Korzemski <wrp40.000@wp.pl>
2025-01-22 21:01:16 +0100

    added wild magic table

--- src/c/RandomTables.tsx
+++ src/c/RandomTables.tsx
@@ -2,12 +2,14 @@ import Layout from "./Layout";
 import Table from "./Table";
 import randomTable from "./../assets/random-table.csv?raw"
 import randomWeaponTable from "./../assets/random-weapon-table.csv?raw"
+import wildMagic from "./../assets/wild-magic.csv?raw"
 import { csvToTableData } from "./../h/csv-to-table-data.helper";
 
 const RandomTables = () => {
 
   const parsedRandomTable = csvToTableData(randomTable);
   const parsedRandomWeaponTable = csvToTableData(randomWeaponTable);
+  const parsedWildMagic = csvToTableData(wildMagic);
     return (
         <Layout>
           <h2> Totally random table </h2>
@@ -17,6 +19,10 @@ const RandomTables = () => {
             <h2> Random weapon generator table </h2>
             <Table
               {...parsedRandomWeaponTable}
+            />
+            <h2>Wild Magic</h2>
+            <Table
+              {...parsedWildMagic}
             />
             <br/>
         </Layout>
