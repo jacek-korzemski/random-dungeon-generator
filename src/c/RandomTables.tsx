c92c492669feb795b22028cae7d850ea71016d80
Jacek Korzemski <wrp40.000@wp.pl>
2025-01-30 19:54:07 +0100

    Some new tables

--- src/c/RandomTables.tsx
+++ src/c/RandomTables.tsx
@@ -3,6 +3,11 @@ import Table from "./Table";
 import randomTable from "./../assets/random-table.csv?raw"
 import randomWeaponTable from "./../assets/random-weapon-table.csv?raw"
 import wildMagic from "./../assets/wild-magic.csv?raw"
+import spellScrolls from "./../assets/spells.csv?raw"
+import events1 from "./../assets/events-1.csv?raw"
+import events2 from "./../assets/events-2.csv?raw"
+import treasures1 from "./../assets/treasures.csv?raw"
+import npc from "./../assets/npc.csv?raw"
 import { csvToTableData } from "./../h/csv-to-table-data.helper";
 
 const RandomTables = () => {
@@ -10,6 +15,11 @@ const RandomTables = () => {
   const parsedRandomTable = csvToTableData(randomTable);
   const parsedRandomWeaponTable = csvToTableData(randomWeaponTable);
   const parsedWildMagic = csvToTableData(wildMagic);
+  const parsedSpellScrolls = csvToTableData(spellScrolls, ";");
+  const parsedEvents1 = csvToTableData(events1, ";");
+  const parsedEvents2 = csvToTableData(events2, ";");
+  const parsedTreasures1 = csvToTableData(treasures1, ";");
+  const parsedNpc = csvToTableData(npc, ";");
     return (
         <Layout>
           <h2> Totally random table </h2>
@@ -23,6 +33,30 @@ const RandomTables = () => {
             <h2>Wild Magic</h2>
             <Table
               {...parsedWildMagic}
+            />
+            <h2>Spell Scrolls</h2>
+            <Table
+              {...parsedSpellScrolls}
+            />
+            <br/>
+            <h2>Random events 1</h2>
+            <Table
+              {...parsedEvents1}
+            />
+            <br/>
+            <h2>Random messages 1</h2>
+            <Table
+              {...parsedEvents2}
+            />
+            <br/>
+            <h2>Random treasures 1</h2>
+            <Table
+              {...parsedTreasures1}
+            />
+            <br/>
+            <h2>Random NPCs</h2>
+            <Table
+              {...parsedNpc}
             />
             <br/>
         </Layout>
--- src/h/csv-to-table-data.helper.ts
+++ src/h/csv-to-table-data.helper.ts
@@ -1,8 +1,8 @@
-export function csvToTableData(csvString: string) {
+export function csvToTableData(csvString: string, separator = ',') {
   const lines = csvString.split("\n");
-  const headers = lines[0].split(",").map(header => ({ key: header.trim() }));
+  const headers = lines[0].split(separator).map(header => ({ key: header.trim() }));
   const rows = lines.slice(1).map(line => {
-      const cells = line.split(",");
+      const cells = line.split(separator);
       return cells.map((cell, index) => ({
           key: headers[index]?.key || `col${index}`,
           content: cell.trim(),
